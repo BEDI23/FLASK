849197cd212b4bf0e54d6c4bb3b449ed087818d6
BEDI <110302864+BEDI23@users.noreply.github.com>
2023-02-10 12:37:17 +0000

    Add files via upload

---
+++ Flaskprojet/docsit/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+@login_required 
+def Accueil():
+    return render_template("Accueil.html", user=current_user)
+    
